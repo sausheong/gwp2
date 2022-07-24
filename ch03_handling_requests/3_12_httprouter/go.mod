28bc34a96de459a56f89fe6279cb44f3b85a2c54
Chang Sau Sheong <sausheong@gmail.com>
2022-07-24 20:48:00 +0800

    uploaded chapter 3 code

---
+++ ch03_handling_requests/3_12_httprouter/go.mod
@@ -0,0 +1,5 @@
+module github.com/sausheong/gwp2/ch03_handling_requests/3_12_httprouter
+
+go 1.18
+
+require github.com/julienschmidt/httprouter v1.3.0
---
+++ ch03_handling_requests/3_13_http2/server.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/net/http2"
+	"net/http"
+)
+
+type MyHandler struct{}
+
+func (h *MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
+}
+
+func main() {
+	handler := MyHandler{}
+	server := http.Server{
+		Addr:    "127.0.0.1:8080",
+		Handler: &handler,
+	}
+	http2.ConfigureServer(&server, &http2.Server{})
+	server.ListenAndServeTLS("cert.pem", "key.pem")
+}
