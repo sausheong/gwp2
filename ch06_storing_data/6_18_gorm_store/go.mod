18cc13f65e1816e0c1ee1630af44fbd951ff178c
Chang Sau Sheong <sausheong@gmail.com>
2022-09-23 03:01:43 +0800

    cleaned up gorm code

--- ch06_storing_data/6_18_gorm_store/go.mod
+++ ch06_storing_data/6_18_gorm_store/go.mod
@@ -3,8 +3,22 @@ module github.com/sausheong/gwp2/ch06_storing_data/6_18_gorm_store
 go 1.18
 
 require (
-	github.com/jinzhu/gorm v1.9.16
 	github.com/lib/pq v1.10.7
+	gorm.io/driver/postgres v1.3.10
+	gorm.io/gorm v1.23.10
 )
 
-require github.com/jinzhu/inflection v1.0.0 // indirect
+require (
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.13.0 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.1 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
+	github.com/jackc/pgtype v1.12.0 // indirect
+	github.com/jackc/pgx/v4 v4.17.2 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.4 // indirect
+	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
--- ch06_storing_data/6_18_gorm_store/store.go
+++ ch06_storing_data/6_18_gorm_store/store.go
@@ -2,26 +2,24 @@ package main
 
 import (
 	"fmt"
-	"time"
 
-	"github.com/jinzhu/gorm"
 	_ "github.com/lib/pq"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 type Post struct {
-	Id        int
-	Content   string
-	Author    string `sql:"not null"`
-	Comments  []Comment
-	CreatedAt time.Time
+	gorm.Model
+	Content  string
+	Author   string
+	Comments []Comment
 }
 
 type Comment struct {
-	Id        int
-	Content   string
-	Author    string `sql:"not null"`
-	PostId    int
-	CreatedAt time.Time
+	gorm.Model
+	Content string
+	Author  string
+	PostID  int
 }
 
 var Db *gorm.DB
@@ -29,7 +27,8 @@ var Db *gorm.DB
 // connect to the Db
 func init() {
 	var err error
-	Db, err = gorm.Open("postgres", "user=gwp dbname=gwp password=gwp sslmode=disable")
+	dsn := "host=localhost user=gwp dbname=gwp password=gwp sslmode=disable"
+	Db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
@@ -37,21 +36,20 @@ func init() {
 }
 
 func main() {
-	post := Post{Content: "Hello World!", Author: "Sau Sheong"}
-	fmt.Println(post) // {0 Hello World! Sau Sheong [] 0001-01-01 00:00:00 +0000 UTC}
-
 	// Create a post
+	post := Post{Content: "Hello World!", Author: "Sau Sheong"}
 	Db.Create(&post)
-	fmt.Println(post) // {1 Hello World! Sau Sheong [] 2015-04-13 11:38:50.91815604 +0800 SGT}
 
 	// Add a comment
 	comment := Comment{Content: "Good post!", Author: "Joe"}
-	Db.Model(&post).Association("Comments").Append(comment)
+	Db.Model(&post).Association("Comments").Append(&comment)
 
-	// Get comments from a post
+	// Get all comments from a post
 	var readPost Post
 	Db.Where("author = $1", "Sau Sheong").First(&readPost)
 	var comments []Comment
-	Db.Model(&readPost).Related(&comments)
-	fmt.Println(comments[0]) // {1 Good post! Joe 1 2015-04-13 11:38:50.920377 +0800 SGT}
+	Db.Model(&readPost).Association("Comments").Find(&comments)
+
+	// show the first comment
+	fmt.Println(comments[0])
 }
